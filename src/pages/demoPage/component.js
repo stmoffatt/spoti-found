495e70b3c51ebb4d5a7a44cc2869c2c14472a3e1
Sean Moffatt <seanmoffatt9@gmail.com>
2019-03-14 16:17:35 -0700

    poster update

--- src/pages/demoPage/component.js
+++ src/pages/demoPage/component.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import DemoGif from '../../images/demo.mp4'
-import GifFrame from '../../images/frame.png'
 import { withRouter } from 'react-router-dom'
 import './demoPage.css'
 
@@ -11,7 +10,7 @@ const DemoPage = ({ history }) => {
   return (
     <div>
       <div className="gifBox">
-        <video loop muted autoPlay poster={GifFrame} alt="SpotiFound-Gif" className="gif">
+        <video loop muted autoPlay preload="none" alt="SpotiFound-Gif" className="gif">
           <source src={DemoGif} type="video/mp4" />
         </video>
       </div>
